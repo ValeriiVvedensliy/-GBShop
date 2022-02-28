5f1415ee954dc4be2b0db65cdaa84d685a73e659
Valerii <valeronmaestro@gmail.com>
2022-02-28 22:43:22 +0200

    refactored

--- GBShop/GBShop/App/Flows/ViewController.swift
+++ GBShop/GBShop/App/Flows/ViewController.swift
@@ -9,42 +9,57 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
+  let auth = AppDelegate.container.resolve(Auth.self)!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let auth = AppDelegate.container.resolve(Auth.self)!
-    
-//    auth.login(userName: "Somebody", password: "mypassword", completionHandler: responseResult)
-//    auth.logout(userId: 123, completionHandler: responseResult)
-//    auth.registerUser(
-//      userName: "Somebody",
-//      password: "mypassword",
-//      email: "some@some.ru",
-//      gender: "m",
-//      creditCard: "9872389-2424-234224-234",
-//      bio: "This is good! I think I will switch to another language",
-//      completionHandler: responseResult
-//    )
-//    auth.changeUser(
-//      userId: 123,
-//      userName: "Somebody",
-//      password: "mypassword",
-//      email: "some@some.ru",
-//      gender: "m",
-//      creditCard: "9872389-2424-234224-234",
-//      bio: "This is good! I think I will switch to another language",
-//      completionHandler: responseResult
-//    )
-//  }
-//
-//  private func responseResult(response: AFDataResponse<LoginResult>) {
-//    switch response.result {
-//    case .success(let login):
-//      print(login)
-//    case .failure(let error):
-//      print(error.localizedDescription)
-//    }
-//  }
+    authRequest()
+    logoutRequest()
+    signUpRequest()
+    editProfileRequest()
+  }
+  
+  private func authRequest() {
+    auth.login(userName: "Somebody", password: "mypassword", completionHandler: responseResult)
+  }
+
+  private func logoutRequest() {
+    auth.logout(userId: 123, completionHandler: responseResult)
+  }
+  
+  private func signUpRequest() {
+    auth.registerUser(
+      userName: "Somebody",
+      password: "mypassword",
+      email: "some@some.ru",
+      gender: "m",
+      creditCard: "9872389-2424-234224-234",
+      bio: "This is good! I think I will switch to another language",
+      completionHandler: responseResult
+    )
+  }
+  
+  private func editProfileRequest() {
+    auth.changeUser(
+      userId: 123,
+      userName: "Somebody",
+      password: "mypassword",
+      email: "some@some.ru",
+      gender: "m",
+      creditCard: "9872389-2424-234224-234",
+      bio: "This is good! I think I will switch to another language",
+      completionHandler: responseResult
+    )
+  }
+  
+  private func responseResult(response: AFDataResponse<LoginResult>) {
+    switch response.result {
+    case .success(let result):
+      print(result)
+    case .failure(let error):
+      print(error.localizedDescription)
+    }
+  }
 }
 
