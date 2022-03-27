daf999cd0a7578f78768d0abab2b1b7baa4845ab
Valerii <valeronmaestro@gmail.com>
2022-03-27 18:20:58 +0300

    feature

--- GBShop/GBShop/App/BusinessLogic/Network/AbstractRequestFactory.swift
+++ GBShop/GBShop/App/BusinessLogic/Network/AbstractRequestFactory.swift
@@ -12,7 +12,7 @@ class AbstractRequestFactory: RequestFactory {
   let errorParser: AbstractErrorParser
   let sessionManager: Session
   let queue: DispatchQueue
-  let baseUrl = URL(string: "https://stormy-refuge-30742.herokpp.com/")
+  let baseUrl = URL(string: "https://stormy-refuge-30742.herokuapp.com/")
   
   init(
     errorParser: AbstractErrorParser,
